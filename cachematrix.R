390df2c1ff6de85e5ffa37dcdecafae64cde8bf3
Derwin McGeary <derwinmcgeary+web@gmail.com>
2015-05-17 00:34:13 +0500

    This is a first stab at solving the problem in Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,33 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+# make a special object which is a matrix which caches its own inverse...
+# ...so that it doesn't take ages to calculate again!
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL # the inverse
+  get <- function() x
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  getsolve <- function() i
+  setsolve <- function(inv) i <<- inv
+  list(set = set, get = get, getsolve = getsolve, setsolve = setsolve)
 }
 
 
 ## Write a short comment describing this function
-
+# if we have already cached x^-1, return that, otherwise calculate the inverse
+# ... and return it
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i <- x$getsolve()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setsolve(i)
+  i
 }
